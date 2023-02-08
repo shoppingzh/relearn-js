c69b72010e926c977cb1250a7c49505a2be84c35
Shopping <xpzheng6@163.com>
2023-02-08 22:31:18 +0800

    学习对象属性迭代

--- object-prop-iterator.js
+++ object-prop-iterator.js
@@ -36,3 +36,62 @@ Object.getOwnPropertyNames(man).forEach(key => console.log(key))
 // 4. Object.getOwnPropertySymbols
 console.log('======== 方法4 ========')
 Object.getOwnPropertySymbols(man).forEach(key => console.log(key))
+
+console.log('========= 获取所有可枚举属性 =========')
+
+function getAllEnumProperties(o) {
+  const list = []
+  for (const key in o) {
+    list.push({
+      name: key,
+      isSelf: Object.hasOwn(o, key)
+    })
+  }
+  return list
+}
+
+getAllEnumProperties(man).forEach(prop => {
+  if (prop.isSelf) {
+    console.log(`自身属性：${prop.name}`)
+  } else {
+    console.log(`来自原型的属性：${prop.name}`)
+  }
+})
+
+console.log('========== 获取所有属性 ==========')
+
+function getAllProperties(o) {
+  if (!o) return []
+  const protoList = [o]
+  let proto = o
+  while ((proto = Object.getPrototypeOf(proto))) {
+    protoList.unshift(proto)
+  }
+  const props = {}
+  const symbolProps = []
+  for (const proto of protoList) {
+    const isSelf = proto === o
+    for (const propName of Object.getOwnPropertyNames(proto)) {
+      const pd = Object.getOwnPropertyDescriptor(proto, propName)
+      props[propName] = {
+        name: propName,
+        isEnumerable: pd.enumerable,
+        isSelf,
+        isSymbol: false,
+      }
+    }
+    for (const symbolName of Object.getOwnPropertySymbols(proto)) {
+      symbolProps.push({
+        name: symbolName,
+        isEnumerable: false,
+        isSelf,
+        isSymbol: true,
+      })
+    }
+  }
+  return [Object.values(props), ...symbolProps]
+}
+
+getAllProperties(man).forEach(prop => {
+  console.log(prop)
+})
