ce17974142205c61ff5717c1eeb906c786a36e67
Shopping <xpzheng6@163.com>
2023-02-13 23:04:08 +0800

    学习检测数组方法

--- is-array.js
+++ is-array.js
@@ -1,5 +1,6 @@
 const arr = [1, 2, 3]
 
+
 // 方法1：instanceOf
 console.log(arr instanceof Array)
 
@@ -9,3 +10,11 @@ console.log(Object.prototype.toString.call(arr) === '[object Array]')
 // 方法3：ES6 Array.isArray
 console.log(Array.isArray(arr))
 
+// 构造函数无关
+const o = Object.create(Array.prototype)
+console.log(o instanceof Array)
+
+const o2 = new Array()
+console.log(o2 instanceof Array)
+Object.setPrototypeOf(o2, Object.prototype)
+console.log(o2 instanceof Array)
